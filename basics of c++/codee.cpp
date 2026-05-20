1141194467520dd124c27c4cd8e11cb98af71f01
amansinha11-dev <sinhaaman473@gnail.com>
2026-05-20 22:13:56 +0530

    Fix multiplication example input prompts

---
+++ basics of c++/codee.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() { 
+    int a;
+    int b;
+    cout << "Enter the value of a : ";
+    cin >> a;
+    cout << "Enter the value of b : ";
+    cin >> b;
+    int mul = a * b;
+    cout<<"The multiplication is that : "<<mul<<endl;    
+    return 0;
+}
