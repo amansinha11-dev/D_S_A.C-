92e145528dea06e8c9820a2e5ca4a6fa1c9456ee
amansinha11-dev <sinhaaman473@gnail.com>
2026-06-09 16:46:02 +0530

    4.2

---
+++ basics of c++/conditionalLoop.cpp/oodEven.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+  int main() {
+   int nums;
+   cout<<"enter your number"<<endl;
+   cin >> nums;
+   if(nums % 2 == 0) {
+     cout<<"Even number"<<endl;
+   } else {
+    cout<<"Odd number"<<endl;
+
+   }
+
+   
+   
+   
+    return 0;
+  }
