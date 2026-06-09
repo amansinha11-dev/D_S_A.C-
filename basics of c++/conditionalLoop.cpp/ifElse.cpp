95c09485af7d7985dcf761af578402ee213cbe8e
amansinha11-dev <sinhaaman473@gnail.com>
2026-06-09 15:06:08 +0530

    new commit

---
+++ basics of c++/conditionalLoop.cpp/ifElse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ int main() {
+    int age;
+    cout<<"Enter Your Age"<<endl;
+    cin>>age;
+    if (age >= 18) {
+        cout <<"Can vote"<<endl;
+    } else {
+        cout<<"You are Minor You Cannot Vote"<<endl;
+    }
+ return 0; }
+ 
