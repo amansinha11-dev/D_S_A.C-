b72ae2624223b63609ede1866f688b2fdb124e43
amansinha11-dev <sinhaaman473@gnail.com>
2026-06-09 15:47:11 +0530

    new program code 4.1

---
+++ basics of c++/conditionalLoop.cpp/largerNumberfind.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+  int main() {
+    int a, b;
+    cout<<"Enter Your Value of A"<<endl;
+    cin >> a;
+    cout <<"Enter B Value"<<endl;
+    cin >> b;
+    if(a >= b) {
+        cout<<"A is Big Value "<< a <<endl;
+    } else { 
+        cout<<"B is Bigger value "<< b <<endl;
+
+    }
+
+
+
+
+
+
+    return 0;
+  }
