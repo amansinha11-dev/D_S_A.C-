18885f957a5a585ef21039ff115191aa7bdfc078
amansinha11-dev <sinhaaman473@gnail.com>
2026-06-09 15:28:52 +0530

    day 4

---
+++ basics of c++/conditionalLoop.cpp/CalculateMarks.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+   int main () {
+    float Marks;
+    cout<<"Enter Your Marks"<<endl;
+    cin>> Marks;
+    if(Marks >= 33.00) {
+        cout<<"You Are Pass"<<endl;
+
+    } else {
+        cout<<"You are Fail Try Next Time"<<endl;
+    }
+    
+
+
+
+
+
+
+
+    return 0;
+   }
--- basics of c++/conditionalLoop.cpp/ifElse.cpp
+++ basics of c++/conditionalLoop.cpp/ifElse.cpp
@@ -5,9 +5,10 @@ using namespace std;
     cout<<"Enter Your Age"<<endl;
     cin>>age;
     if (age >= 18) {
-        cout <<"Can vote"<<endl;
-    } else {
+        cout <<"You Can vote"<<endl;
+    } if(age >=25) {cout<<"You Can Participate election"<<endl;}
+      else {
         cout<<"You are Minor You Cannot Vote"<<endl;
     }
  return 0; }
- 
+ ////mulrtiple timess if use kar shakta hai But else only 1 time wo bhi last meh 
