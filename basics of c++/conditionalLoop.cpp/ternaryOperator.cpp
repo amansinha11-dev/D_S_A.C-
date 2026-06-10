cd165f9ee09d7aa2ba2d33dafde6c63cb7c4429a
amansinha11-dev <amansinha11-dev@users.noreply.github.com>
2026-06-10 14:55:47 +0530

    5.1

---
+++ basics of c++/conditionalLoop.cpp/ternaryOperator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main () {
+int a, b;
+//ternary operator
+cin >> a;
+cin >> b;
+int largest= a >= b ? a:b;
+cout << "largest is " << largest << endl; /// yah par cout kar ke largest ko output -pe print kar rahee hai
+
+
+
+
+
+return 0;
+
+
+
+
+
+}
