c246a64179c95b50ce94b1bd04a4e26df632e52b
amansinha11-dev <sinhaaman473@gmail.com>
2026-08-06 23:19:48 +0530

    latest commit

---
+++ basics of c++/conditionalLoop.cpp/loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your values"<<endl;
+    cin>>n;
+    for(int i = 0; i < n; i++) {
+        cout<<"Jai Durga Maa"<<endl;
+
+    }
+   return 0;
+}
