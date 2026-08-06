9c055b31c111bf5ff9954cfc7226cb7a921dc3ab
amansinha11-dev <sinhaaman473@gmail.com>
2026-08-06 23:10:40 +0530

    revisioncode

---
+++ basics of c++/conditionalLoop.cpp/revisionloops.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+   int n;
+   cout<<"enter Your N : "<<endl;
+   cin>>n;
+   for(int i = 0;i<n;i++){
+    cout<<"aman sinha"<<endl;
+  } //r(int i = 7; i<9;i++) {
+    //ut<<"sinha"<<endl;
+  
+ return 0;
+}
