50f2eca817727a738cc89a33f4bfbe2cec4c9c01
amansinha11-dev <sinhaaman473@gmail.com>
2026-08-07 23:09:16 +0530

    pattrer2

--- basics of c++/striver/functionParameter.cpp
+++ basics of c++/striver/functionParameter.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // Function to calculate the sum using parameters a and b
-int sum(int a, int b) {
+int sum(int a, int b = 1) {
     int sum = a + b;
     return sum;
 }
@@ -15,14 +15,14 @@ int sub(int a, int b) {
 
 int main() {
     // Calling the sum function with arguments 2 and 4
-    int s1 = sum(2, 4);
+    int s1 = sum(2);
     cout << "sum : " << s1 << endl;
 
     // Calling the sub function with arguments 6 and 3
-    int s2 = sub(6, 3);
+   // int s2 = sub(6, 3);
     
     // Correctly printing the variable s2
-    cout << "Your difference value is : " << s2 << endl;
+    //cout << "Your difference value is : " << s2 << endl;
 
     return 0;
 }
---
+++ basics of c++/striver/functionParameter1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int prod(int a, int b){
+   int prod = a * b;}
+
+bool isEven(int n) {
+    if(n % 2 == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main(){
+   // int p = prod(2, 3);
+    cout<<isEven(20)<<endl;
+    return 0;
+}
