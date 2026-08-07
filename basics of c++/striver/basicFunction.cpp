29a5ffee024d277c77634619f49e75cb32b79ee2
amansinha11-dev <sinhaaman473@gmail.com>
2026-08-07 20:02:32 +0530

    functionbasic

---
+++ basics of c++/striver/basicFunction.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printName(string name) {
+    cout<<"Hey "<<name;
+}
+int main() {
+    string name;
+    cout<<"Enter your name"<<endl;
+    cin >> name;
+    printName(name);
+    return 0;
+
+}
